cb7b9c7217e3bd372bbca4a76da5bb853705a507
Fomin Mykola <giraffe.in.ua@gmail.com>
2017-07-31 08:15:29 +0300

    Initial Commit

---
+++ AdaptiveDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveDemo
+//
+//  Created by Fomin Mykola on 7/31/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
